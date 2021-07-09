17ffe9693db474cc1ebce703e5c82d83c2c0c819
bateso88 <bateso@hotmail.co.uk>
2021-07-09 17:11:16 +0100

    Build bitcoin price tracker

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About World</h1>
+      <p>About bla bla bla</p>
+    </div>
+  </Layout>
+);
+
+export default About;
